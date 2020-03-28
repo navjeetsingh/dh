1e09560f0e3967eaa663eb2e3b8a155b02264bf9
Navjeet Singh <navjeet.singh5@yahoo.com>
2020-03-29 00:39:46 +0700

    Created collection preview component map over the data displayed on the shop page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/homepage.component';
+import ShopPage from './pages/ShopPage/ShopPage.component';
 import './App.css';
 
 function App() {
 	return (
 		<div>
 			<Switch>
-				 <Route exact path="/" component={() => <HomePage />} />
+				<Route exact path="/" component={() => <HomePage />} />
+				<Route path="/shop" component={() => <ShopPage />} />
 			</Switch>
 		</div>
 	);
---
+++ src/components/CollectionPreview/CollectionPreview.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './CollectionPreview.styles.scss';
+
+const CollectionPreview = ({ title, items }) => (
+	<div className="collection-preview">
+		<h1 className="title">{title.toUpperCase()}</h1>
+		<div className="preview">
+			{items
+				.filter((item, index) => index < 4)
+				.map(({ id, name }) => (
+					<div key={id}>{name}</div>
+				))}
+		</div>
+	</div>
+);
+
+export default CollectionPreview;
