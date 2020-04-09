ff4ca2fa2ddfab0d5b2a25001a89953dc4ddf96b
Navjeet Singh <navjeet.singh5@yahoo.com>
2020-04-09 22:41:59 +0700

    moved directory data to redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -3,6 +3,7 @@ import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import userReducer from './user/user.reducer.js';
 import cartReducer from './cart/cart.reducer.js';
+import directoryReducer from './directory/directory.reducer.js'
 
 const persistConfig = {
 	key: 'root',
@@ -13,6 +14,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
 	user: userReducer,
 	cart: cartReducer,
+	directory: directoryReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
