8f3bb9ea87f02018c33a03371c8410dad05d5486
Navjeet Singh <navjeet.singh5@yahoo.com>
2020-04-15 22:41:35 +0700

    Implementing redux-thunk

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -51,21 +51,23 @@ export const addCollectionAndDocuments = async (collectionKey, objectToAdd) => {
 	return await batch.commit();
 };
 
-export const convertCollectionSnapshotToMap = (collections) => {
-	const transformedCollection = collections.docs.map((doc) => {
-		const { title, items } = doc.data();
-		return {
-			routeName: encodeURI(title.toLowerCase()),
-			id: doc.id,
-			title,
-			items,
-		};
-	});
+export const convertCollectionsSnapshotToMap = (collections) => {
+	const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
 
 	return transformedCollection.reduce((accumulator, collection) => {
-		accumulator[collection.title.toLowerCase()] = collection;
-		return accumulator;
-	}, {});
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
 };
 
 export const auth = firebase.auth();
